e4e0d1edaa800065210d0d8415cbbc4a17cac925
Raghu Betina <rbetina@gmail.com>
2013-07-22 08:23:46 -0500

    Broke a lot of stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rxngif::Application.routes.draw do
   root "pictures#index"
   get "pictures" => "pictures#index"
   get "pictures/new" => "pictures#new"
-  get "pictures/:id" => "pictures#show"
-  get "create_picture" => "pictures#create"
+  get "pictures/:number" => "pictures#show"
+  get "create_picture", "pictures#create"
 end
