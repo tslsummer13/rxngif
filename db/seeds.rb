e30b7b900c56f710158bfb920d4a9bbc51a9e8e8
Raghu Betina <rbetina@gmail.com>
2013-07-22 09:29:52 -0500

    Switched over to optional hash syntax everywhere

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@
 Picture.destroy_all
 
 pictures = [
-  { :source => "http://www.w3.org/html/logo/downloads/HTML5_Logo_512.png", :caption => "HTML5 Logo" },
-  { :source => "http://upload.wikimedia.org/wikipedia/commons/f/f1/Ruby_logo.png", :caption => "Ruby logo" },
-  { :source => "http://upload.wikimedia.org/wikipedia/commons/1/16/Ruby_on_Rails-logo.png", :caption => "Rails logo" },
-  { :source => "http://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png", :caption => "JavaScript logo" }
+  { source: "http://www.w3.org/html/logo/downloads/HTML5_Logo_512.png", caption: "HTML5 Logo" },
+  { source: "http://upload.wikimedia.org/wikipedia/commons/f/f1/Ruby_logo.png", caption: "Ruby logo" },
+  { source: "http://upload.wikimedia.org/wikipedia/commons/1/16/Ruby_on_Rails-logo.png", caption: "Rails logo" },
+  { source: "http://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png", caption: "JavaScript logo" }
 ]
 10.times do
   pic = pictures.sample
