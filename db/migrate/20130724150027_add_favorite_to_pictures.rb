aef761d2d530019ebaeafa1cdbe5641e4a5c6ce0
Raghu Betina <rbetina@gmail.com>
2013-07-24 10:23:41 -0500

    Migration challenge

--- db/migrate/20130724150027_add_favorite_to_pictures.rb
+++ db/migrate/20130724150027_add_favorite_to_pictures.rb
@@ -3,3 +3,12 @@ class AddFavoriteToPictures < ActiveRecord::Migration
     add_column :pictures, :favorite, :boolean
   end
 end
+
+# If you haven't already, add a column called "favorite" of type "boolean"
+#  to the pictures table.
+
+# Then use this as a guide to add a "favorite" column of type "boolean"
+#   to the videos table
+
+# Remember, the first step to changing your DB is always:
+#   rails generate migration <you pick a name>
