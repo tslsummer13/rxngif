d2d1be11c9aec8e20b4c63f2e907b91634252221
Raghu Betina <rbetina@gmail.com>
2013-07-24 10:28:21 -0500

    Updated migration challenge with hints

--- db/migrate/20130724150027_add_favorite_to_pictures.rb
+++ db/migrate/20130724150027_add_favorite_to_pictures.rb
@@ -8,7 +8,9 @@ end
 #  to the pictures table.
 
 # Then use this as a guide to add a "favorite" column of type "boolean"
-#   to the videos table
+#   to the videos table. Create the table if you don't have it yet.
+#   Shortcut syntax to create a model:
+#     rails generate model video source:string caption:string
 
 # Remember, the first step to changing your DB is always:
 #   rails generate migration <you pick a name>
